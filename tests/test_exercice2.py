409eb0d90f698b473300d7a9fd5e49074b417ebb
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-03-17 15:25:44 +0100

    Update test_exercice2.py

--- tests/test_exercice2.py
+++ tests/test_exercice2.py
@@ -1,4 +1,4 @@
 from exercices.exercice2 import *
 
-def test_tri_iteratif():
-    assert tri_iteratif([41, 55, 21, 18, 12, 6, 25]) == [6, 12, 18, 21, 25, 41, 55]
+def test_fusion(L1, L2):
+    assert fusion([1, 6, 10], [0, 7, 8, 9]) == [0, 1, 6, 7, 8, 9, 10]
