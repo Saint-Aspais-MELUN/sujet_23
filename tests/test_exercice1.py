67e80aae428b5d5931803ce864d6b33b8349454e
COILHAC <62248137+Mireille77@users.noreply.github.com>
2021-03-18 18:34:59 +0100

    Update test_exercice1.py

--- tests/test_exercice1.py
+++ tests/test_exercice1.py
@@ -1,5 +1,5 @@
 from exercices.exercice1 import *
 
-def test_occurence_lettres(phrase):
+def test_occurence_lettres():
     phrase = "Hello world !"
     assert occurence_lettres(phrase) == {'H': 1, 'e': 1, 'l': 3, 'o': 2, ' ': 2, 'w': 1, 'r': 1, 'd': 1, '!': 1}
